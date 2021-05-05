aa4c49c0d7aa2c36eb20bf95f165118aebccf911
mutiaramahee <mutiaramahee17@gmail.com>
2021-05-05 08:51:23 +0700

    handle errors of form validation

--- resources/views/event_organizer_template/eo_template.blade.php
+++ resources/views/event_organizer_template/eo_template.blade.php
@@ -121,6 +121,18 @@
                 </div>
             @endif
 
+            @if ($errors->any())
+                <div class='alert alert-danger'>
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h4><i class="icon fa fa-danger"></i> Whoops!</h4>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
         <!-- Your Page Content Here -->
             @yield('content')
         </section><!-- /.content -->
