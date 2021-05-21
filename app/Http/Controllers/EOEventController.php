a2bd7596043e707e1721613e4190d2ec3610267a
mutiaramahee <mutiaramahee17@gmail.com>
2021-05-21 09:32:02 +0700

    dd email

--- app/Http/Controllers/EOEventController.php
+++ app/Http/Controllers/EOEventController.php
@@ -109,17 +109,17 @@ class EOEventController extends Controller
             $data['due'] = date('F d, Y', strtotime("+1 week"));
             $data['event_name'] = $request->input('name');
 
-            Mail::send('mail.invoice', $data, function($message) {
-                $message->to($data['email'], Session::get('admin_name'))
-                        ->subject('Invoice from Draw System');
-                $message->from('draw.eventy@gmail.com', 'Draw System');
+            // Mail::send('mail.invoice', $data, function($message) {
+            //     $message->to($data['email'], Session::get('admin_name'))
+            //             ->subject('Invoice from Draw System');
+            //     $message->from('draw.eventy@gmail.com', 'Draw System');
 
             });
-
-            if (Mail::failures()) {
-                CRUDBooster::redirect(URL::to('eo/event'), "The event has been added! But failed when sending email about payment info. Contact administrator for payment info", "warning");
-            } 
-            CRUDBooster::redirect(URL::to('eo/event'), "The event has been added! Please check your email to get the payment info", "info");
+            dd($data['email'], $user->email);
+            // if (Mail::failures()) {
+            //     CRUDBooster::redirect(URL::to('eo/event'), "The event has been added! But failed when sending email about payment info. Contact administrator for payment info", "warning");
+            // } 
+            // CRUDBooster::redirect(URL::to('eo/event'), "The event has been added! Please check your email to get the payment info", "info");
 
         }
     }
